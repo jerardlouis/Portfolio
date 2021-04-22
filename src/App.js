e718660fea91c0270b8dc85352f0ebe1c90a661a
Jerard Dayanghirang <jerardlguevarra@gmail.com>
2021-04-22 19:44:23 +0000

    fixed headshot

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Socials from './Socials.js';
+import headshot from './headshot.jpg'
 function App() {
   return (
     <div className="App">
-      <img class='headshot' src="https://github.com/jerardlouis/Resume-Site/blob/main/src/headshot.jpg?raw=true"/>
+      <img class='headshot' src={headshot}/>
+      
       <h1>Jerard Louis Dayanghirang Guevarra</h1>
       <p>Software Engineer | Musician</p>
       <p>Based in NYC</p>
