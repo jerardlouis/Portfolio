d2b8ac54be78a60db31787fa0a954df8f421d9be
Jerard Dayanghirang Guevarra <jerardlguevarra@gmail.com>
2021-08-19 14:33:07 -0400

    Updated resume

--- src/Socials.js
+++ src/Socials.js
@@ -43,7 +43,7 @@ function Socials(){
            height="25"
         />
         </a >
-        <a href="https://drive.google.com/file/d/1vFj_TARwGP9N1HM2TcT69JwX6oJS0mgL/view" target="_blank">
+        <a href="https://drive.google.com/file/d/1amL4MDvFLPYCmBN1Eze4X7fyDvo2B38Y/view?usp=sharing" target="_blank">
          <img class="email" src="https://i.imgur.com/GoBT7iu.png"
            width="25"
            height="25"
