d7ef2c62fe0f9e92ecc06f5b954e79d6b15f92bd
Jerard Guevarra <jerardlguevarra@gmail.com>
2024-02-15 14:32:09 -0800

    update icon and resume

--- src/Socials.js
+++ src/Socials.js
@@ -43,7 +43,7 @@ function Socials(){
            height="25"
         />
         </a >
-        <a href="https://drive.google.com/file/d/1amL4MDvFLPYCmBN1Eze4X7fyDvo2B38Y/view?usp=sharing" target="_blank">
+        <a href="https://drive.google.com/file/d/1qmTIR7mz4BTylg3DK-aedy6GlbZwrXgP/view?usp=sharing" target="_blank">
          <img class="email" src="https://i.imgur.com/GoBT7iu.png"
            width="25"
            height="25"
