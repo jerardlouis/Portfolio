4ed4ed5b7e6060a36b377dfc5b1cb7a9a8c681d0
Jerard Guevarra <jerardlguevarra@gmail.com>
2024-03-07 20:23:21 -0800

    making it pretty

--- src/Socials2.js
+++ src/Socials2.js
@@ -14,6 +14,10 @@ function Socials2(){
             />
             </a >
             <p>LinkedIn</p>
+            <img class="linkedin" src="https://cdn1.iconfinder.com/data/icons/social-media-circle-7/512/Circled_Linkedin_svg-512.png"
+            width="25"
+            height="25"
+            />
             </div>
             <div class="link">
             <a href="mailto: jeardlguevarra@gmail.com" target="_blank">
@@ -23,6 +27,10 @@ function Socials2(){
             />
             </a >
             <p>Email</p>
+            <img class="email" src="https://cdn4.iconfinder.com/data/icons/rounded-white-basic-ui/139/Mail02-RoundedWhite-512.png"
+            width="25"
+            height="25"
+            />
             </div>
             <div class="link">
             <a href="https://drive.google.com/file/d/1vkXfC-ECoObVyVFNRifbFx6TCDeP3NeY/view?usp=sharing" target="_blank">
@@ -32,6 +40,10 @@ function Socials2(){
             />
             </a >
             <p>Resume</p>
+            <img class="email" src="https://i.imgur.com/GoBT7iu.png"
+            width="25"
+            height="25"
+            />
             </div>
             <div class="link">
             <a href="https://github.com/jerardlouis" target="_blank">
@@ -41,6 +53,10 @@ function Socials2(){
             />
             </a >
             <p>GitHub</p>
+            <img class="github" src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png"
+                width="25"
+                height="25"
+            />
             </div>
             <div class="link">  
             <a href="https://www.instagram.com/jerardlouis/?hl=en" target="_blank">
@@ -50,6 +66,10 @@ function Socials2(){
             />
             </a>
             <p>Instagram</p>
+            <img class="instagram" src="https://cdn1.iconfinder.com/data/icons/feather-2/24/instagram-512.png"
+            width="25"
+            height="25"
+            />
             </div>
             <div class="link">
             <a href="https://open.spotify.com/user/222efgwx2w3kei4ibnolmncyi?si=82ee7ebd769c4277" target="_blank">
@@ -59,6 +79,10 @@ function Socials2(){
             />
             </a >
             <p>Spotify</p>
+            <img class="spotify" src="https://cdn0.iconfinder.com/data/icons/social-glyph/30/spotify-480.png"
+            width="25"
+            height="25"
+            />
             </div>
             <div class="link">
             <a href="https://www.tiktok.com/@jerardlouis/video/6875032236828216582?lang=en&is_copy_url=1&is_from_webapp=v2" target="_blank">
@@ -68,6 +92,10 @@ function Socials2(){
             />
             </a >
             <p>TikTok</p>
+            <img class="tiktok" src="https://cdn0.iconfinder.com/data/icons/social-media-2310/24/_tik_tok_communication_social_media_network-512.png"
+            width="25"
+            height="25"
+            />
             </div>  
             
             
