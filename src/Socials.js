575d0753b2fb347f72ffe3e4801308b4b7451184
Jerard Guevarra <jerardlguevarra@gmail.com>
2024-05-22 15:28:11 -0700

    removing unused social media pages

--- src/Socials.js
+++ src/Socials.js
@@ -17,12 +17,8 @@ function Socials(){
            height="25"
          />
          </a >
-        <a href="https://www.tiktok.com/@jerardlouis/video/6875032236828216582?lang=en&is_copy_url=1&is_from_webapp=v2" target="_blank">
-         <img class="tiktok" src="https://cdn0.iconfinder.com/data/icons/social-media-2310/24/_tik_tok_communication_social_media_network-512.png"
-           width="25"
-           height="25"
-         />
-         </a >
+        
+        
           
         <a href="https://github.com/jerardlouis" target="_blank">
         <img class="github" src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png"
@@ -31,24 +27,14 @@ function Socials(){
         />
         </a >
           
-        <a href="https://www.instagram.com/jerardlouis/?hl=en" target="_blank">
-         <img class="instagram" src="https://cdn1.iconfinder.com/data/icons/feather-2/24/instagram-512.png"
-           width="25"
-           height="25"
-        />
-        </a>
+      
         <a href="https://www.linkedin.com/in/jerard-louis-dayanghirang-guevarra-514717139/" target="_blank">
          <img class="linkedin" src="https://cdn1.iconfinder.com/data/icons/social-media-circle-7/512/Circled_Linkedin_svg-512.png"
            width="25"
            height="25"
         />
         </a >
-        <a href="https://twitter.com/gothasianboy" target="_blank">
-         <img class="twitter" src="https://upload.wikimedia.org/wikipedia/commons/9/95/Twitter_new_X_logo.png?20230802142353"
-           width="25"
-           height="25"
-        />
-        </a >
+        
         <a href="mailto: jeardlguevarra@gmail.com" target="_blank">
          <img class="email" src="https://cdn4.iconfinder.com/data/icons/rounded-white-basic-ui/139/Mail02-RoundedWhite-512.png"
            width="25"
--- src/Socials2.js
+++ src/Socials2.js
@@ -58,19 +58,7 @@ function Socials2(){
             />
             </div>
             </a >
-            <a href="https://www.instagram.com/jerardlouis/?hl=en" target="_blank">
-            <div class="link">  
-            <img class="instagram" src="https://cdn1.iconfinder.com/data/icons/feather-2/24/instagram-512.png"
-            width="25"
-            height="25"
-            />
-            <p>Instagram</p>
-            <img class="instagram" src="https://cdn.iconscout.com/icon/free/png-512/free-ellipsis-horizontal-menu-grid-collection-44652.png?f=webp&w=256"
-            width="25"
-            height="25"
-            />
-            </div>
-            </a>
+            
             <a href="https://open.spotify.com/user/222efgwx2w3kei4ibnolmncyi?si=82ee7ebd769c4277" target="_blank">
             <div class="link">
             <img class="spotify" src="https://cdn0.iconfinder.com/data/icons/social-glyph/30/spotify-480.png"
@@ -83,19 +71,6 @@ function Socials2(){
             height="25"
             />
             </div>
-            </a >
-            <a href="https://www.tiktok.com/@jerardlouis/video/6875032236828216582?lang=en&is_copy_url=1&is_from_webapp=v2" target="_blank">
-            <div class="link">
-            <img class="tiktok" src="https://cdn0.iconfinder.com/data/icons/social-media-2310/24/_tik_tok_communication_social_media_network-512.png"
-            width="25"
-            height="25"
-            />
-            <p>TikTok</p>
-            <img class="tiktok" src="https://cdn.iconscout.com/icon/free/png-512/free-ellipsis-horizontal-menu-grid-collection-44652.png?f=webp&w=256"
-            width="25"
-            height="25"
-            />
-            </div>  
             </a >
             <a href="https://venmo.com/u/JerardLouis" target="_blank">
             <div class="link">
@@ -110,20 +85,6 @@ function Socials2(){
             />
             </div>  
             </a >
-            <a href="https://twitter.com/gothasianboy" target="_blank">
-            <div class="link">
-            <img class="tiktok" src="https://upload.wikimedia.org/wikipedia/commons/9/95/Twitter_new_X_logo.png?20230802142353"
-            width="25"
-            height="25"
-            />
-            <p>The App Formerly Known as Twitter</p>
-            <img class="tiktok" src="https://cdn.iconscout.com/icon/free/png-512/free-ellipsis-horizontal-menu-grid-collection-44652.png?f=webp&w=256"
-            width="25"
-            height="25"
-            />
-            </div>  
-            </a >
-            
             <a href="https://sleepy-falls-48470-0ab41f492d84.herokuapp.com/" target="_blank">
             <div class="link">
             <img class="tiktok" src="https://www.vhv.rs/dpng/d/430-4306427_settings-gear-settings-gear-icon-free-hd-png.png"
